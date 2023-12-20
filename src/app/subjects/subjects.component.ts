9473d3a578c50f96ae863d1835ac7075e0cfcb46
MarwaSaaed <marwasaeed373@gmail.com>
2023-12-20 21:47:09 +0200

    Maroo

---
+++ src/app/subjects/subjects.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { ISubject } from '../Shared-Interfase/ISubject';
+import { SubjectService } from '../Servise/subject.service';
+
+@Component({
+  selector: 'app-subjects',
+  templateUrl: './subjects.component.html',
+  styleUrls: ['./subjects.component.scss']
+})
+export class SubjectsComponent {
+  subjects:ISubject[]=[]
+  constructor(private serv:SubjectService)
+  {
+
+  }
+  ngOnInit(): void {
+  this.serv.GetAll().subscribe(
+    {
+      next:data=>{
+          this.subjects=data
+      },
+      error:err=>console.log(err)
+    }
+  )
+  }
+  DeleteSubject(id: number = 0) {
+    this.serv.Delete(id).subscribe
+     (
+        () => {
+  
+          console.log('Before deletion:', this.subjects);
+          this.subjects = this.subjects.filter((sub) => sub.id !== id);
+          console.log('After deletion:', this.subjects);
+        },
+        (error) => {
+          console.error('Error deleting Subject:', error);
+  
+        }
+      );
+  }
+  
+}
